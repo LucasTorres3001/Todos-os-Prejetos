761be7f1ffaa253ad376e91a896760bf41d313e8
Lucas Aparecido Torres <lukas.torres30.lt@gmail.com>
2021-03-02 21:18:36 -0300

    Add-New-Project
    Projeto semestral da Universidade.

---
+++ Projeto/funcoes.js
@@ -0,0 +1,45 @@
+function mudaFoto(foto){
+	document.getElementById("imagem").src=foto;
+}
+function mudaIcone(icone){
+	document.getElementById("icone").src=icone;
+}
+function mudaImagem(imagem){
+	document.getElementById("Fale").src=imagem;
+}
+function mudaPaisagem(paisagem){
+	document.getElementById("paisagem").src=paisagem;
+}
+function mudaImagens(imagens){
+	document.getElementById("imagens").src=imagens;
+}
+function mudaAbc(abc){
+	document.getElementById("abc").src=abc;
+}
+function mudaOi(oi){
+	document.getElementById("oi").src=oi;
+}
+function mudaIo(io){
+	document.getElementById("io").src=io;
+}
+function mudaIdent(ident){
+	document.getElementById("ident").src=ident;
+}
+function mudaPaintium(paintium){
+	document.getElementById("paintium").src=paintium;
+}
+function mudaMuda(womans){
+	document.getElementById('womans').src=womans;
+}
+function mudaDeg(saint){
+	document.getElementById('saint').src=saint;
+}
+function mudaLol(furious){
+	document.getElementById('furious').src=furious;
+}
+function mudaLeg(boff){
+	document.getElementById('boff').src=boff;
+}
+function mudaArm(sufle){
+	document.getElementById('sufle').src=sufle;
+}
